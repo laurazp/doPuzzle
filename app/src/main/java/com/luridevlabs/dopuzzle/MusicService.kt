fabcfdc4afa1a5cba5ac84846506c5a4f60102bb
laurazp <laurikazp@gmail.com>
2023-02-08 23:32:11 +0000

    MusicService created to manage background music

---
+++ app/src/main/java/com/luridevlabs/dopuzzle/MusicService.kt
@@ -0,0 +1,13 @@
+package com.luridevlabs.dopuzzle
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MusicService: Service() {
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+}
